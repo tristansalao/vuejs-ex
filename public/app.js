2c97cd462fda2e6dbe8bdf69d41d66fac5acf5eb
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-06-22 19:08:31 +0800

    kunwari may binago

--- public/app.js
+++ public/app.js
@@ -1,6 +1,44 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello World! Your first Vue.js Application on Digital Garage'
+  el: '#app',
+  created: function(){
+      this.getData()
+  },
+  methods: {
+      getData: function(){
+                        var vm = this;
+                        $.ajax({
+                            url: 'https://jsonplaceholder.typicode.com/posts',
+                            method: 'GET'
+                        }).then(function (data) {
+                            vm.items = data;     
+                        });                             
+      }
+  },
+  data () {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'No.',
+          align: 'left',
+          sortable: false,
+          value: 'no'
+        },
+        { text: 'HO ID', value: 'hoid', sortable: false },
+        { text: 'Commission', value: 'commission' },
+        { text: 'Deposit', value: 'deposit' },
+        { text: 'Deposit(Transfer)', value: 'deptransfer' },
+        { text: 'Withdraw', value: 'withdraw' },
+        { text: 'Withdraw(Transfer)', value: 'withtransfer' },
+        { text: 'Betting Amount', value: 'betamount' },
+        { text: 'Rake', value: 'rake' },
+        { text: 'User Holding Money', value: 'userholding' },
+        { text: 'Operating Head Office Profit', value: 'offprofit' },
+        { text: 'Head Office Money', value: 'offmoney' },
+        { text: 'Money of Low Rank', value: 'moneylowrank' },
+        { text: 'Low Rank', value: 'lowrank' },
+      ],
+      items: []
     }
+  }
 });
